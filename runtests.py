de973b5235968f6b4cf2142000abaebe00048269
Federico Ferri <federico.ferri.it@gmail.com>
2017-08-06 01:46:56 +0200

    refactor test suite

--- runtests.py
+++ runtests.py
@@ -6,16 +6,36 @@ verbose=False
 for c in argv[1:]:
     exec(c)
 
-def runtest(name, expected_exc=None, expected_output=None, params={}):
-    filename = list(iglob('tests/test*%s*' % name))
-    if len(filename) < 1:
+def readfile(n, prefix, strip=False):
+    filenames = list(iglob('tests/*%s*/%s' % (n, prefix)))
+    if len(filenames) < 1:
+        return None
+    if len(filenames) > 1:
+        print('warning: %s matches multiple tests. running %s...' % (name, filenames[0]))
+    with open(filenames[0], 'r') as f:
+        return {False: lambda x: x, True: lambda x: x.strip()}[strip](f.read())
+
+def runtest(name):
+    input_str = readfile(name, 'input*')
+    if input_str is None:
         raise RuntimeError('test not found: %s' % name)
-    if len(filename) > 1:
-        print('warning: %s matches multiple tests. running %s...' % (name, filename[0]))
-    with open(filename[0], 'r') as f:
-        input_str = f.read()
     if verbose:
-        print('RUNNING TEST %s' % filename[0])
+        print('RUNNING TEST %s' % name)
+    expected_output = readfile(name, 'expected_output*')
+    if verbose and expected_output is not None:
+        print('TEST HAS EXPECTED OUTPUT')
+    expected_exc = readfile(name, 'expected_exception', True)
+    if verbose and expected_exc is not None:
+        print('TEST HAS EXPECTED EXCEPTION')
+    params = {}
+    params_s = readfile(name, 'params')
+    if params_s is not None:
+        for x in params_s.split('\n'):
+            if x == '': continue
+            if verbose:
+                print('SETTING PARAM %s' % x)
+            k, v = x.split('=', 1)
+            params[k] = v
     import pycpp
     try:
         p = pycpp.PyCPP(input_str)
@@ -31,24 +51,18 @@ def runtest(name, expected_exc=None, expected_output=None, params={}):
                 print('error: test %s failed (does not match expected output)' % name)
                 exit(1)
     except Exception as e:
+        en = e.__class__.__name__
         if verbose:
-            print('EXCEPTION: %s' % e)
+            print('EXCEPTION: %s' % en)
         if expected_exc is None:
             print('error: test %s failed (was not expected to fail with %s)' % (name, type(e)))
             exit(1)
-        if type(e) != expected_exc:
-            print('error: test %s failed (failed with %s but was expected to fail with %s)' % (name, type(e), expected_exc))
+        if en != expected_exc:
+            print('error: test %s failed (failed with %s but was expected to fail with %s)' % (name, en, expected_exc))
             exit(1)
 
-runtest('001')
-runtest('002')
-runtest('003', None, '''struct Test {
-    int a;
-    float b;
-};
+for i in iglob('tests/*'):
+    i=i.split('/')[1]
+    runtest(i)
 
-''', dict(a='int', b='float'))
-runtest('004', RuntimeError)
-runtest('005', NameError)
-runtest('006')
 print('all test passed successfully')
---
+++ tests/003-params/expected_output.cpp
@@ -0,0 +1,5 @@
+struct Test {
+    int a;
+    float b;
+};
+
