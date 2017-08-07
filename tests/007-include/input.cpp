31c6f758d0fc5ef2dda032daa108d5a6271a4bb4
Federico Ferri <federico.ferri.it@gmail.com>
2017-08-07 16:51:19 +0200

    use include params feature

--- tests/007-include/input.cpp
+++ tests/007-include/input.cpp
@@ -7,8 +7,5 @@
 )
 
 #py for k,v in d.items():
-#py pycpp.params['name'] = k
-#py pycpp.params['type'] = v[0]
-#py pycpp.params['num_args'] = v[1]
-#py pycpp.include('tmpl')
+#py pycpp.include('tmpl', {'name': k, 'type': v[0], 'num_args': v[1]})
 #py endfor
